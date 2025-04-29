09dc37eebf8b34fb70c55194bed49c0936548907
frmnxhken <frmnxhken@gmail.com>
2025-04-29 18:17:48 +0700

    Init setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h1 className='text-4xl'>
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
