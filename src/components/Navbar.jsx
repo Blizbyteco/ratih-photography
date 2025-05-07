9c46f61a188dd797a69cf77ae56625671a1170bf
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-07 21:21:58 +0700

    UPDATE: Added navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from "react";
+import { easeIn, motion } from "motion/react";
+
 
 export default function Navbar() {
+
+  const variants = {
+    initial: {
+      opacity: 0
+    },
+
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 0.8,
+        delay: 1.3,
+        ease: easeIn
+      }
+    }
+  }
+
   return (
-    <div className="absolute left-0 top-0 w-full z-20">
+    <motion.div variants={variants} initial="initial" animate="animate" className="absolute left-0 top-0 w-full z-20">
       <div className="max-w-[1200px]  mx-auto flex justify-between items-center">
         <img src="/images/logo.svg" alt="logo" className="size-[150px]" />
         <div className="flex gap-x-12">
-          <a href="" className="text-white">
+          <a href="#" className="text-white">
             Galeri
             <sup className="text-sm">(6)</sup>
           </a>
-          <a href="" className="text-white">
+          <a href="#" className="text-white">
             Kursus
           </a>
-          <a href="" className="text-white">
+          <a href="#" className="text-white">
             Layanan Kami
           </a>
         </div>
@@ -35,6 +53,6 @@ export default function Navbar() {
           Menu
         </button>
       </div>
-    </div>
+    </motion.div>
   );
 }
