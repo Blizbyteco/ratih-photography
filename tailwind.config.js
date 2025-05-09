0e41583c6bde1a3b99e80828e0e6cb3767eb452b
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-09 15:11:27 +0700

    UPDATE: Moved extended font into configuration file for cleaner

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+      },
+      fontStyle: {
+        italic: 'italic',
+        normal: 'normal',
+      },
+    },
+  },
+  plugins: [],
+};
