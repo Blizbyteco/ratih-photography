bec04f24a16c6214a59aaf1141c646a564adf97d
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-09 15:11:07 +0700

    UPDATE: Moved navbar to become fixed and visible on scolling approximately 20% of the page

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-import { easeIn, easeOut, motion } from "motion/react";
-
+import React, { useEffect, useState } from "react";
+import {
+  easeIn,
+  easeOut,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "motion/react";
+import Button from "./Button";
 
 export default function Navbar() {
-
   const variants = {
     initial: {
-      opacity: 0
+      opacity: 0,
     },
 
     animate: {
@@ -14,79 +19,156 @@ export default function Navbar() {
       transition: {
         duration: 0.8,
         delay: 1.3,
-        ease: easeIn
-      }
-    }
-  }
+        ease: easeIn,
+      },
+    },
+  };
+
+  const fixedNavVariants = {
+    initial: (i) => ({
+      y: "120%",
+      transition: {
+        duration: 0.2,
+        ease: easeOut,
+        delay: i / 10,
+      },
+    }),
+
+    animate: (i) => ({
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: easeOut,
+        delay: (i / 10) + 0.3,
+      },
+    }),
+  };
 
   const backgroundVariants = {
     initial: {
       y: "-100%",
       transition: {
         duration: 0.8,
-        ease: easeIn
-      }
+        ease: easeIn,
+      },
     },
 
     animate: {
       y: 0,
       transition: {
         duration: 0.8,
-        ease: easeOut
-      }
-    }
-  }
+        ease: easeOut,
+      },
+    },
+  };
 
+  const { scrollYProgress } = useScroll();
 
   const [isActive, setIsActive] = useState(false);
+  const [showNavBtn, setShowNavBtn] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest < 0.05) {
+      setShowNavBtn(false);
+    } else {
+      setShowNavBtn(true);
+    }
+  });
+
+  useEffect(() => {
+    if (isActive) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "initial";
+    }
+  }, [isActive]);
 
   return (
-    <motion.div variants={variants} initial="initial" animate="animate" className="absolute left-0 top-0 w-full z-20">
-
-     
-
-
-      <div className=" max-w-[1200px] mx-auto flex justify-between items-center py-4 z-[999]">
-        <img src="/images/logo.svg" alt="logo" className="aspect-video w-[150px]" />
-        <div className="flex gap-x-12">
-          <a href="#" className="text-white">
-            Galeri
-            <sup className="text-sm">(6)</sup>
-          </a>
-          <a href="#" className="text-white">
-            Kursus
-          </a>
-          <a href="#" className="text-white">
-            Layanan Kami
-          </a>
+    <>
+      <motion.div
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        className="absolute left-0 top-0 w-full z-50"
+      >
+        <div className="max-w-[1200px] mx-auto flex justify-between items-center py-4">
+          <img
+            src="/images/logo.svg"
+            alt="logo"
+            className="aspect-video w-[150px]"
+          />
+          <div className="flex gap-x-12">
+            <a href="#" className="text-white">
+              Galeri
+              <sup className="text-sm">(6)</sup>
+            </a>
+            <a href="#" className="text-white">
+              Kursus
+            </a>
+            <a href="#" className="text-white">
+              Layanan Kami
+            </a>
+          </div>
+          <Button text="Kontak kami" />
         </div>
-        <button onClick={() => setIsActive(state => !state)} className="flex gap-x-2 text-white hover:cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="white"
-            className="size-6"
+      </motion.div>
+
+      {/* fixed navbar */}
+      <div className="fixed top-0 left-0 min-w-full bg-transparent z-40">
+        <div className="max-w-[1200px] mx-auto flex justify-end items-center gap-x-4 mt-8 overflow-y-hidden">
+          <motion.div
+            variants={fixedNavVariants}
+            initial="initial"
+            animate={showNavBtn ? "animate" : "initial"}
+            custom={1}
+            >
+            <Button text="Kontak kami" />
+          </motion.div>
+          <motion.button
+            variants={fixedNavVariants}
+            initial="initial"
+            animate={showNavBtn ? "animate" : "initial"}
+            custom={2}
+            onClick={() => setIsActive(true)}
+            className="flex items-center gap-x-2 bg-white text-black hover:cursor-pointer px-4 rounded-full py-2 text-sm font-semibold"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
-            />
-          </svg>
-          Menu
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="black"
+              className="size-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+              />
+            </svg>
+            Menu
+          </motion.button>
+        </div>
       </div>
 
-       {/* background */}
-      <motion.div
-      variants={backgroundVariants}
-      initial="initial"
-      animate={isActive ? "animate" : "initial"}
-      className="absolute w-full min-h-screen top-0 left-0 bg-black -z-10">
-        </motion.div>
       {/* background */}
-    </motion.div>
+      <motion.div
+        variants={backgroundVariants}
+        initial="initial"
+        animate={isActive ? "animate" : "initial"}
+        className="fixed min-w-full min-h-screen top-0 left-0 bg-black z-60"
+      >
+        <div className="max-w-[1200px] mx-auto flex justify-between items-center">
+          <img
+              src="/images/logo.svg"
+              alt="logo"
+              className="aspect-video w-[150px]"
+            />
+
+            <button onClick={() => setIsActive(false)} className="text-white hover:cursor-pointer">Close</button>
+        </div>
+      </motion.div>
+      {/* background */}
+    </>
   );
 }
