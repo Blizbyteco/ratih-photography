476e882db37ab30c27304fc7a3881a3cfc43ba59
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-09 11:31:23 +0700

    UPDATE: Added navbar animation

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { easeIn, motion } from "motion/react";
+import React, { useState } from "react";
+import { easeIn, easeOut, motion } from "motion/react";
 
 
 export default function Navbar() {
@@ -19,9 +19,34 @@ export default function Navbar() {
     }
   }
 
+  const backgroundVariants = {
+    initial: {
+      y: "-100%",
+      transition: {
+        duration: 0.8,
+        ease: easeIn
+      }
+    },
+
+    animate: {
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: easeOut
+      }
+    }
+  }
+
+
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <motion.div variants={variants} initial="initial" animate="animate" className="absolute left-0 top-0 w-full z-20">
-      <div className="max-w-[1200px] mx-auto flex justify-between items-center py-4">
+
+     
+
+
+      <div className=" max-w-[1200px] mx-auto flex justify-between items-center py-4 z-[999]">
         <img src="/images/logo.svg" alt="logo" className="aspect-video w-[150px]" />
         <div className="flex gap-x-12">
           <a href="#" className="text-white">
@@ -35,7 +60,7 @@ export default function Navbar() {
             Layanan Kami
           </a>
         </div>
-        <button className="flex gap-x-2 text-white">
+        <button onClick={() => setIsActive(state => !state)} className="flex gap-x-2 text-white hover:cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -53,6 +78,15 @@ export default function Navbar() {
           Menu
         </button>
       </div>
+
+       {/* background */}
+      <motion.div
+      variants={backgroundVariants}
+      initial="initial"
+      animate={isActive ? "animate" : "initial"}
+      className="absolute w-full min-h-screen top-0 left-0 bg-black -z-10">
+        </motion.div>
+      {/* background */}
     </motion.div>
   );
 }
