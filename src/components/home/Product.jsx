4dd779db6c61e90ba8ee374ae5e40c7f322660f5
mfhaisall <mhfaisal011@gmail.com>
2025-05-21 12:17:57 +0700

    menegubah teks menjadi hasil foto kami

--- src/components/home/Product.jsx
+++ src/components/home/Product.jsx
@@ -5,9 +5,9 @@ const Product = () => {
   return (
     <section className="relative z-20 w-full min-h-screen bg-black text-white">
       <div className="grid grid-cols-2 px-8 py-32 gap-y-6">
-        <h2 className="text-8xl col-span-2 font-semibold">Produk Unggulan</h2>
-        <h2 className="text-8xl font-semibold text-right">Kami</h2>
-        <h2 className="text-8xl font-semibold text-right">(06)</h2>
+        <h2 className="text-8xl col-span-2 font-semibold">HASIL FOTO</h2>
+        <h2 className="text-8xl font-semibold text-right">KAMI</h2>
+        <h2 className="text-8xl font-semibold text-right">06</h2>
       </div>
 
       <div className="grid grid-cols-2 px-8 py-12 gap-y-12">
